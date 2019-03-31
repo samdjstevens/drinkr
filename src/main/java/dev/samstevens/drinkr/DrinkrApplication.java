d32368f515fa3314b60bc04efd22a0d903f8ac11
Sam Stevens <sam@ticketline.co.uk>
2019-03-31 19:01:29 +0100

    generate project with Spring Initializr

---
+++ src/main/java/dev/samstevens/drinkr/DrinkrApplication.java
@@ -0,0 +1,13 @@
+package dev.samstevens.drinkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkrApplication.class, args);
+	}
+
+}
